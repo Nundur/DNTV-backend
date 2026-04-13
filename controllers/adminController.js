30cc6d74a004ef36c86218d8d53b65d0dc9688f5
Nundur <retinandi2006@gmail.com>
2026-04-13 22:19:45 +0200

    admin routokat bővítettem bulkUpdate-al, amivel egyszerre több user-t tudok módosítani, egy módosított userekből álló tömböt vár

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {config} = require('../config/dotenvConfig')
-const {allUsers} = require('../models/adminModels.js')
+const {allUsers, bulkUpdate} = require('../models/adminModels.js')
 const { rollback } = require('../db/db.js')
 
 
@@ -16,6 +16,18 @@ const getAllUsers = async (req, res)=>{
     }
 }
 
+const bulkUpdateUsers = async (req, res)=>{
+    try {
+        const users = req.body
+
+        const result = await bulkUpdate(users)
+
+        return res.status(200).json(result)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({error: 'adatbázis hiba', err})
+    }
+}
 
 
 
@@ -23,4 +35,4 @@ const getAllUsers = async (req, res)=>{
 
 
 
-module.exports = {getAllUsers};
+module.exports = {getAllUsers, bulkUpdateUsers};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const {register, login, whoAmI, logout} = require('../controllers/userController.js')
 const {authAdmin} = require('../middleware/adminMiddleware.js');
-const { getAllUsers } = require('../controllers/adminController.js');
+const { getAllUsers, bulkUpdateUsers } = require('../controllers/adminController.js');
 
 const ruter = express.Router();
 
 ruter.get('/getAllUsers', authAdmin, getAllUsers)
+ruter.post('/bulk-update-users', authAdmin, bulkUpdateUsers)
 
 
 
